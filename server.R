792f696b966576b00e5fe1b0cdc195ded8ec7c14
guillaumelobet <g.lobet@fz-juelich.de>
2018-02-07 15:49:39 +0100

    update the shiny version

--- server.R
+++ server.R
@@ -69,6 +69,28 @@ shinyServer(
     
     observe({
       if(is.null(rs$lipids)){return()}
+      vars <- unique(rs$lipids$leaf_zone)
+      ct_options <- list()
+      sel <- input$zone_to_plot
+      if(length(sel) == 0) sel = vars
+      for(ct in vars) ct_options[[ct]] <- ct
+      # cts  <- c("tot_root_length","n_laterals","tot_lat_length")
+      updateSelectInput(session, "zone_to_plot", choices = ct_options, selected=sel) 
+    }) 
+    
+    observe({
+      if(is.null(rs$lipids)){return()}
+      vars <- unique(rs$lipids$genotype)
+      ct_options <- list()
+      sel <- input$genotypes_to_plot_2
+      if(length(sel) == 0) sel = vars
+      for(ct in vars) ct_options[[ct]] <- ct
+      # cts  <- c("tot_root_length","n_laterals","tot_lat_length")
+      updateSelectInput(session, "genotypes_to_plot_2", choices = ct_options, selected=sel) 
+    })     
+    
+    observe({
+      if(is.null(rs$lipids)){return()}
       vars <- unique(rs$lipids$variable)
       ct_options <- list()
       sel <- input$to_plot_2
@@ -101,6 +123,29 @@ shinyServer(
       if(length(sel) == 0 | sel == "") sel = ct_options[1]
       # cts  <- c("tot_root_length","n_laterals","tot_lat_length")
       updateSelectInput(session, "to_plot_3", choices = ct_options, selected=sel) 
+    })  
+    
+    observe({
+      if(is.null(rs$lipids)){return()}
+      vars <- colnames(rs$lipids)[c(2:10)]
+      ct_options <- list()
+      sel <- input$to_plot_4
+      for(ct in vars) ct_options[[ct]] <- ct
+      if(length(sel) == 0 | sel == "") sel = ct_options[1]
+      # cts  <- c("tot_root_length","n_laterals","tot_lat_length")
+      updateSelectInput(session, "to_plot_4", choices = ct_options, selected=sel) 
+    })  
+    
+    observe({
+      if(is.null(rs$lipids)){return()}
+      vars <- unique(rs$lipids$variable)
+      ct_options <- list()
+      sel <- input$variable_to_pca
+      for(ct in vars) ct_options[[ct]] <- ct
+      if(is.null(sel)) sel = ct_options
+      if(length(sel) == 0 | sel == "") sel = ct_options
+      # cts  <- c("tot_root_length","n_laterals","tot_lat_length")
+      updateSelectInput(session, "variable_to_pca", choices = ct_options, selected=sel) 
     })     
     
     ############################################################
@@ -109,23 +154,98 @@ shinyServer(
     
     # Time plot
     
+    
+    temp <- data %>%
+      mutate(value = scale(value))
+    
+    ggplot(temp, aes(leaf_zone, variable)) + 
+      geom_tile(aes(fill=value),colour = "white") + 
+      scale_fill_gradient(low = "white", high = "steelblue")
+    
+    
     output$my_plot <- renderPlot({
 
-      temp <- rs$lipids
-      temp <- temp[temp$genotype %in% input$genotypes_to_plot,]
-      temp <- temp[temp$variable == input$to_plot_2,]
+      temp <- rs$lipids %>%
+        filter(genotype %in% input$genotypes_to_plot)%>%
+        filter(leaf_zone %in% input$zone_to_plot)%>%
+        filter(variable == input$to_plot_2)
+      # temp <- data
+
       temp$value <- as.numeric(temp$value)
+      temp$var <- temp[[input$to_plot]]
+      temp$var2 <- factor(temp[[input$to_plot_3]])
       
-      print(str(temp))
-      
-      pl <- ggplot(temp, aes_string(x=input$to_plot, y= "value", color = input$to_plot_3)) + 
-        geom_jitter(width = 0.25) + 
-        theme_classic()
+      if(!input$isboxplot){
+        pl <- ggplot(temp, aes(x=var, y= value, color = var2)) +
+          geom_jitter(width = 0.25) + 
+          theme_classic()
+      }else{
+        pl <- ggplot(temp, aes(x=var, y= value, color = var2)) + 
+          geom_boxplot(width = 0.25) + 
+          theme_classic()
+        
+        # ggplot(temp, aes(x=genotype, y= value, color=factor(dev_stage))) + 
+        #   geom_boxplot() + 
+        #   theme_classic()
+      }
       pl
       
     }) 
+      
     
-    
+    # PCA PLOT
+    output$pca_plot <- renderPlot({
+      if(is.null(rs$lipids)){return()}
+      
+      temp <- rs$lipids[rs$lipids$genotype %in% input$genotypes_to_plot_2,]
+      samples <- temp$sample_id
+      genotypes <- temp$genotype
+      vars <- input$variable_to_pca
+      cats <- colnames(temp)
+      for( i in c(1:2)) cats <- cats[-length(cats)]
+      temp <- filter(temp, variable == vars)
+      temp <- dcast(temp, cats ~ variable)
+      vars <- vars[vars %in%colnames(temp)]
+      temp <- temp[,vars]
+      
+      # remove <- NULL
+      # for(i in c(1:ncol(temp))){
+      #   if(sd(temp[,i]) == 0) remove <- c(remove,i)
+      # }
+      
+      # if(!is.null(remove)) temp <- temp[,-remove]
+      
+      str(temp)
+      
+      pca <- prcomp(temp, retx = T, scale=T)  # Make the PCA
+      pca.results <- cbind(plant=plants, genotype=genotypes, data.frame(pca$x)[,])
+      
+      vars <- apply(pca$x, 2, var)  
+      props <- round((vars / sum(vars) * 100), 1)
+      xl <- paste0("\nPrincipal Component 1 (",props[1],"%)")
+      yl <-paste0("Principal Component 2 (",props[2],"%)\n")
+      
+      pl1 <- ggplot(data = pca.results) + 
+        geom_point(aes(PC1, PC2, colour=genotype)) +
+        stat_ellipse(aes(PC1, PC2, colour=genotype), level = 0.9, size=1) + 
+        theme_bw() + 
+        xlab(xl) + 
+        ylab(yl)
+      
+      z2 <- data.frame(var_names = rownames(pca$rotation), pca$rotation[, 1:2])
+      z2$var_names <- gsub("_", " ", z2$var_names)
+      
+      pl2 <- ggplot(data=z2, aes(0, 0, xend=PC1, yend=PC2)) + 
+        geom_segment(col="grey", size=1.2, arrow = arrow(length = unit(0.5,"cm")), alpha=0.9) +
+        geom_text_repel(data=z2, aes(PC1, PC2, label=var_names), col="black", size=9) +
+        geom_point(aes(x=0, y=0), colour="grey") +
+        #scale_y_continuous(limits = c(-1, 0.3)) +
+        theme_classic() +
+        xlab(xl) + ylab(yl)
+      
+      pl <- grid.arrange(pl1, pl2, ncol=1)
+      
+    })
     
     
     ############################################################
--- ui.R
+++ ui.R
@@ -18,23 +18,51 @@ shinyUI(fluidPage(
         )
       )
     ),
-    tabPanel("Plot data", id="tab2", icon = icon('sliders'),
+    tabPanel("Plot individual data", id="tab2", icon = icon('sliders'),
       fluidRow(
         column(3, 
                h4("Plot"),
                helpText("Choose the genotypes, variables, lipid classes and colours to map"),
-               selectInput("genotypes_to_plot", label="Genotypes to plot", choices = c("Load datafile"), 
-                           selected = NULL, multiple = TRUE, width="100%"),
-               selectInput("to_plot", label = "Variable to plot", choices = c("")),
-               selectInput("to_plot_2", label = "Value to plot", choices = c("")),
-               selectInput("to_plot_3", label = "Colours to plot", choices = c(""))
+               selectInput("to_plot", label = "Grouping variable", choices = c("")),
+               selectInput("to_plot_3", label = "Coloring variable", choices = c("")),
+               tags$hr(),
+               checkboxInput("isboxplot", label="Want boxplot instead?"),
+               tags$hr(),
+               tags$hr(),
+               checkboxInput("more_filters", label="More filters?", value = F),
+               conditionalPanel(
+                 condition = "input.more_filters == true",
+                 selectInput("genotypes_to_plot", label="Filter genotypes", choices = c("Load datafile"), 
+                             selected = NULL, multiple = TRUE, width="100%"),
+                 selectInput("zone_to_plot", label="Filter leaf zones", choices = c("Load datafile"), 
+                             selected = NULL, multiple = TRUE, width="100%")
+               )
         ),
         column(8,
-               
+               selectInput("to_plot_2", label = "Variable to plot", choices = c("")),
                plotOutput("my_plot")
         )
       )
+    ),
+    tabPanel("Principal component analysis", id="tab2", icon = icon('bullseye'),
+       fluidRow(
+         column(3, 
+                helpText("Principal component analysis of the whole dataset"),
+                selectInput("genotypes_to_plot_2", label="Genotypes to plot", choices = c("Load datafile"), 
+                            selected = NULL, multiple = TRUE, width="100%"),
+                selectInput("to_plot_4", label = "Colours to plot", choices = c("")),
+                selectInput("variable_to_pca", label="Variables to include in PCA", choices = c("Load datafile"), 
+                            selected = NULL, multiple = TRUE, width="100%"),
+                tags$hr()         
+        ),
+         column(7,
+                h4("Plot the principal component analysis"),
+                tags$hr(),
+                plotOutput("pca_plot", height = 800)
+         )
+       )
     )
+    
              
   )
 ))
