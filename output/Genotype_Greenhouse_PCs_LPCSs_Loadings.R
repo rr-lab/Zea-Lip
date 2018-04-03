d078b572aaff2e0d6fd5f8e0f16cc59f38d21075
Karla Juárez <karla.juarez@ira.cinvestav.mx>
2018-04-02 22:48:05 -0500

    LPCs PCs loadings for field and greenhouse

---
+++ output/Genotype_Greenhouse_PCs_LPCSs_Loadings.R
@@ -0,0 +1,14 @@
+InitDataObjects("conc", "stat", FALSE)
+mSet<-Read.TextData(mSet, "Replacing_with_your_file_path", "rowu", "disc");
+mSet<-SanityCheckData(mSet)
+mSet<-ReplaceMin(mSet);
+mSet<-Normalization(mSet, "NULL", "LogNorm", "ParetoNorm", "1", ratio=FALSE, ratioNum=20)
+mSet<-PlotNormSummary(mSet, "norm_0_", "png", 72, width=NA)
+mSet<-PlotSampleNormSummary(mSet, "snorm_0_", "png", 72, width=NA)
+mSet<-SPLSR.Anal(mSet, 5, 10, "same")
+mSet<-PlotSPLSPairSummary(mSet, "spls_pair_0_", "png", 72, width=NA, 5)
+mSet<-PlotSPLS2DScore(mSet, "spls_score2d_0_", "png", 72, width=NA, 1,2,0.95,1,0)
+mSet<-PlotSPLS3DScore(mSet, "spls_score3d_0_", "json")
+mSet<-PlotSPLSLoading(mSet, "spls_loading_0_", "png", 72, width=NA, 1,"overview");
+mSet<-PlotSPLSDA.Classification(mSet, "spls_cv_0_", "Mfold", "png", 72, width=NA)
+mSet<-PlotSPLSLoading(mSet, "spls_loading_0_", "png", 600, width=NA, 1,"overview");
